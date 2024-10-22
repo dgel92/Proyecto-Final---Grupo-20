34e72740e706d4f92c827248502112cce7bae142
Nahir Ferreyra <nahirferreyrad@gmail.com>
2024-10-22 10:53:14 -0300

    conexion bd y modulo accion

--- programacion/dao/accion_dao.py
+++ programacion/dao/accion_dao.py
@@ -1,1 +1,25 @@
-nahir
+from db.conn_db import conectar_bd
+
+class AccionDAO:
+    @staticmethod
+    def obtener_por_codigo(codigo):
+        conn = conectar_bd()
+        cursor = conn.cursor(dictionary=True)
+        cursor.execute("SELECT * FROM acciones WHERE codigo = %s", (codigo,))
+        accion = cursor.fetchone()
+        cursor.close()
+        conn.close()
+        return accion
+
+    @staticmethod
+    def actualizar_cantidad(codigo, cantidad):
+        conn = conectar_bd()
+        cursor = conn.cursor()
+        try:
+            cursor.execute("UPDATE acciones SET cantidad_disponible = %s WHERE codigo = %s", (cantidad, codigo))
+            conn.commit()
+        except mysql.connector.Error as err:
+            print("Error al actualizar la cantidad de acciones: {}".format(err))
+        finally:
+            cursor.close()
+            conn.close()
