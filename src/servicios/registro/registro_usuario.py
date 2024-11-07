13155351f86bfbd857b7710d153b06c26fb8c6b3
daniel gonzalez <dgel92@gmail.com>
2024-11-06 21:40:02 -0300

    se arregla problema de seguridad, se permitia cambiar contraseña sin responder pregunta de seguridad

--- src/servicios/registro/registro_usuario.py
+++ src/servicios/registro/registro_usuario.py
@@ -1,10 +1,10 @@
 import bcrypt
-import mysql.connector  # Asegúrate de importar este módulo
+import mysql.connector
 from src.conn.db_conn import connect_to_mysql
 from src.servicios.broker.menu_broker import menu_principal
 
 
-def registrar_usuario(nombre, email, password):
+def registrar_usuario(nombre, email, password, pregunta_seguridad, respuesta_seguridad):
     connection = connect_to_mysql()
     if connection:
         cursor = connection.cursor()
@@ -17,8 +17,15 @@ def registrar_usuario(nombre, email, password):
             hashed_password = bcrypt.hashpw(password.encode("utf-8"), bcrypt.gensalt())
 
             cursor.execute(
-                "INSERT INTO inversores (nombre, email, contraseña, saldo) VALUES (%s, %s, %s, %s)",
-                (nombre, email, hashed_password, 1000000),
+                "INSERT INTO inversores (nombre, email, contraseña, saldo, pregunta_seguridad, respuesta_seguridad) VALUES (%s, %s, %s, %s, %s, %s)",
+                (
+                    nombre,
+                    email,
+                    hashed_password,
+                    1000000,
+                    pregunta_seguridad,
+                    respuesta_seguridad,
+                ),
             )
             connection.commit()
             print("Usuario registrado con éxito.")
@@ -62,20 +69,41 @@ def iniciar_sesion(email, password):
             connection.close()
 
 
-def recuperar_contrasena(email, nueva_password):
+def recuperar_contrasena(email):
     connection = connect_to_mysql()
     try:
         if connection:
             cursor = connection.cursor()
-            hashed_password = bcrypt.hashpw(
-                nueva_password.encode("utf-8"), bcrypt.gensalt()
-            )
             cursor.execute(
-                "UPDATE inversores SET contraseña = %s WHERE email = %s",
-                (hashed_password, email),
+                "SELECT pregunta_seguridad, respuesta_seguridad FROM inversores WHERE email = %s",
+                (email,),
             )
-            connection.commit()
-            print("Contraseña actualizada con éxito.")
+            usuario = cursor.fetchone()
+            if not usuario:
+                print("Error: El email ingresado no es valido.")
+                return
+
+            pregunta = usuario[0]
+            respuesta_correcta = usuario[1]
+
+            print(f"Porfavor ingresa la respuesta a la pregunta de seguridad")
+            respuesta_usuario = input("Ingrese la respuesta: ")
+
+            if respuesta_usuario.lower() == respuesta_correcta.lower():
+                nueva_password = input("Ingrese su nueva contraseña: ")
+
+                hashed_password = bcrypt.hashpw(
+                    nueva_password.encode("utf-8"), bcrypt.gensalt()
+                )
+
+                cursor.execute(
+                    "UPDATE inversores SET contraseña = %s WHERE email = %s",
+                    (hashed_password, email),
+                )
+                connection.commit()
+                print("Contraseña actualizada con éxito.")
+            else:
+                print("Respuesta incorrecta a la pregunta de seguridad.")
     except mysql.connector.Error as err:
         print(f"Error al actualizar la contraseña: {err}")
     finally:
@@ -94,14 +122,17 @@ if __name__ == "__main__":
         nombre = input("Ingrese su nombre: ")
         email = input("Ingrese su email: ")
         password = input("Ingrese su contraseña: ")
-        registrar_usuario(nombre, email, password)
+        pregunta_seguridad = input("Ingrese una pregunta de seguridad: ")
+        respuesta_seguridad = input("Ingrese la respuesta a la pregunta de seguridad: ")
+        registrar_usuario(
+            nombre, email, password, pregunta_seguridad, respuesta_seguridad
+        )
     elif opcion == "2":
         email = input("Ingrese su email: ")
         password = input("Ingrese su contraseña: ")
         iniciar_sesion(email, password)
     elif opcion == "3":
         email = input("Ingrese su email: ")
-        nueva_password = input("Ingrese su nueva contraseña: ")
-        recuperar_contrasena(email, nueva_password)
+        recuperar_contrasena(email)
     else:
         print("Opción no válida.")
